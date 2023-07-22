d2a3cdaff5aab605225183cb0bc92a3260b540e9
EKANSH <ekanshh2011@gmail.com>
2023-07-22 16:43:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require("path");
+const path = require('path');
 
 const app = express();
 const mongoose = require('mongoose');
@@ -10,54 +10,51 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // MongoDB connection URI
 // const mongoURI = 'mongodb://localhost:27017';
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Corrected 'port' variable to 'PORT'
 
 mongoose.set('strictQuery', false);
-const connectDB = async() => {
+
+const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
     process.exit(1);
   }
-}
-
+};
 
 const loginSchema = new mongoose.Schema({
-    email: String,
-    password: String,
-  });
-  
-const login = mongoose.model( 'login', loginSchema);
+  email: String,
+  password: String,
+});
 
-// EXPRESS SPECIFIC STUFF   
-app.use('/static', express.static('static')) // For serving static files
+const login = mongoose.model('login', loginSchema);
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')); // For serving static files
 app.use(express.urlencoded({ extended: true }));
 
 // Serve the login page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Handle login form submission
-app.post("/", (req, res) => {
-    var myData = new login(req.body);
-    myData.save().then(() => {
-      res.send("This item has been saved to the database")
-    }).catch(() => {
-      res.status(400).send("item was not saved to the database")
+app.post('/', (req, res) => {
+  var myData = new login(req.body);
+  myData
+    .save()
+    .then(() => {
+      res.send('This item has been saved to the database');
+    })
+    .catch(() => {
+      res.status(400).send('Item was not saved to the database');
     });
-  })
+});
 
-//   // Validate the email and password
-//   if (email === 'user@example.com' && password === 'password123') {
-//     res.send('Login successful!');
-//   } else {
-//     res.send('Invalid email or password. Please try again.');
-//   }
-// });
+// Validate the email and password
+// Commenting out this block since it's not necessary for the current implementation.
 
 // const connectDB = async() => {
 //   try {
@@ -70,13 +67,15 @@ app.post("/", (req, res) => {
 //   }
 // }
 
-connectDB().then(() =>{
-  app.listen(PORT, () =>{
+connectDB().then(() => {
+  app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
-  })
+  });
 });
 
 // Start the server
+// Commenting out this block since the server is already started in the connectDB() function.
+
 // const PORT = 3000;
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
